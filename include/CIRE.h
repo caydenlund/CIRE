8cd6b20cdae2ff212078c453418fdb312a515250
tirpankartanmay@gmail.com <tirpankartanmay@gmail.com>
2023-10-04 15:45:09 -0600

    Checkpoint for next change

---
+++ include/CIRE.h
@@ -0,0 +1,30 @@
+//
+// Created by tanmay on 10/2/23.
+//
+
+#ifndef CIRE_CIRE_H
+#define CIRE_CIRE_H
+
+#include<string>
+#include<map>
+#include "parser.h"
+
+// Give Flex the prototype of yylex we want ...
+#define YY_DECL int yylex(Graph *graph)
+// ... and declare it for the parser's sake.
+YY_DECL;
+
+// Conducting the whole scanning and parsing of Calc++.
+class CIRE {
+public:
+  Graph *graph;
+  CIRE();
+
+  // Run the parser on file F.  Return 0 on success.
+  int parse (const char &f);
+
+  // The name of the file being parsed.
+  std::string file;
+};
+
+#endif //CIRE_CIRE_H
