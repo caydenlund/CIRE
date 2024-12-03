1f6735e9dbd314d1040ad328f8efaefdbf836078
tanmaytirpankar <tirpankartanmay@gmail.com>
2024-12-02 19:09:10 -0700

    Added loop to validator

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,6 +1,55 @@
 import os
 import sys
+import glob
 import json
+import shutil
+import functools
+
+# Do not assign any value to this variable. Script will find the path to clang.
+CLANG_PATH = ""
+
+
+# Finds clang in the specified directory or in the PATH
+def find_clang(directory):
+    """Finds the path to clang in the specified directory."""
+
+    if directory:
+        for root, _, files in os.walk(directory):
+            for file in files:
+                if file == "clang":
+                    return os.path.join(root, file)
+
+    # If clang is not found in the specified directory, search the PATH
+    if shutil.which("clang"):
+        return shutil.which("clang")
+    else:
+        return "clang not found"
+        exit(0)
+
+
+# This function runs clang on every .c/.cpp file in the specified directory and outputs LLVM IR
+# to another directory in files with the same name but with .ll extension
+def run_clang_on_dir(directory, flag_list):
+    # Get all the files in the directory
+    files = os.listdir(directory)
+    directory_extension = functools.reduce(lambda x, y: x + "_" + y, flag_list, "")
+    destination_dir = directory + directory_extension
+
+    flags = functools.reduce(lambda x, y: x + " -" + y, flag_list, "")
+
+    # Iterate over all the files
+    for file in files:
+        # Check if the file is a .c or .cpp file
+        if file.endswith(".c"):
+            # Run clang on the file
+            print("Running clang on " + directory + '/' + file)
+            os.system(CLANG_PATH + "clang" + flags + " -S -emit-llvm " + directory + '/' + file + " -o " +
+                      destination_dir + '/' + file[:-2] + ".ll")
+        elif file.endswith(".cpp"):
+            # Run clang++ on the file
+            print("Running clang++ on " + directory + '/' + file)
+            os.system(CLANG_PATH + "clang++" + flags + " -S -emit-llvm " + directory + '/' + file + " -o " +
+                      destination_dir + '/' + file[:-4] + ".ll")
 
 
 # This function summarises the json file results
@@ -34,24 +83,40 @@ def run_cire_llvm(llvm_file):
 # This function runs CIRE_LLVM on every .ll file in specified directory
 def run_cire_llvm_on_dir(directory):
     # Get all the files in the directory
-    files = os.listdir(directory)
+    files = glob.glob(directory + '/*.ll')
 
     os.system("rm results.json default.log")
 
     # Iterate over all the files
     for file in files:
-        # Check if the file is a .ll file
-        if file.endswith(".ll"):
-            # Run CIRE_LLVM on the file
-            # print("Running CIRE_LLVM on " + directory + '/' + file)
-            run_cire_llvm(directory + '/' + file)
+        # Run CIRE_LLVM on the file
+        # print("Running CIRE_LLVM on " + directory + '/' + file)
+        run_cire_llvm(file)
 
     summarize("results.json")
 
 
 # main function
 def main():
+    # Provide path to the directory containing the .c/.cpp files
     directory = sys.argv[1]
+    clang_search_path = ""
+    # Provide directory containing clang if it is not in the PATH
+    if len(sys.argv) > 2:
+        clang_search_path = sys.argv[2]
+
+    # Provide flags to pass to clang
+    flag_list = ["O3", "ffast-math"]
+
+    global CLANG_PATH
+    CLANG_PATH = os.path.dirname(find_clang(clang_search_path))
+    print("Clang found at: " + CLANG_PATH)
+
+    # run_clang_on_dir(directory, flag_list)
+
+    # directory_extension = functools.reduce(lambda x, y: x + "_" + y, flag_list, "")
+    # destination_dir = directory + directory_extension
+
     # Run CIRE_LLVM on specified directory
     run_cire_llvm_on_dir(directory)
 
--- src/VALIDATOR.cpp
+++ src/VALIDATOR.cpp
@@ -15,6 +15,98 @@
 
 #include <iostream>
 #include <llvm/IRReader/IRReader.h>
+#include <cmath>
+
+// Function to create higher precision shadow instructions from a function to another function
+void createShadowFunction(llvm::Function *srcFunction, llvm::Function *destFunction) {
+  llvm::LLVMContext &context = srcFunction->getContext();
+  llvm::IRBuilder<> builder(context);
+
+  // Create a mapping of arguments from the source function to the destination function
+  llvm::ValueToValueMapTy VMap;
+  for (auto &arg : srcFunction->args()) {
+    llvm::Argument *newArg = &*destFunction->arg_begin() + arg.getArgNo();
+    VMap[&arg] = newArg;
+  }
+
+  // Clone the basic blocks of the source function to the destination function
+  for (auto &srcBlock : *srcFunction) {
+    llvm::BasicBlock *destBlock = llvm::BasicBlock::Create(context, srcBlock.getName(), destFunction);
+    VMap[&srcBlock] = destBlock;
+
+
+    // Clone the instructions of the source basic block to the destination basic block
+    for (auto &srcInst : srcBlock) {
+      llvm::Value *clonedInst;
+      switch (srcInst.getOpcode()) {
+        case llvm::Instruction::FAdd:
+        case llvm::Instruction::FSub:
+        case llvm::Instruction::FMul:
+        case llvm::Instruction::FDiv:
+        case llvm::Instruction::FRem: {
+          clonedInst = llvm::BinaryOperator::Create(
+                  static_cast<llvm::Instruction::BinaryOps>(srcInst.getOpcode()), VMap[srcInst.getOperand(0)], VMap[srcInst.getOperand(1)], "", destBlock);
+          }
+          break;
+        case llvm::Instruction::FPExt: {
+          if (srcInst.getType() == builder.getHalfTy()) {
+            clonedInst = builder.CreateFPExt(VMap[srcInst.getOperand(0)], builder.getFloatTy());
+          } else if (srcInst.getType() == builder.getFloatTy()) {
+            clonedInst = builder.CreateFPExt(VMap[srcInst.getOperand(0)], builder.getDoubleTy());
+          } else {
+            std::cerr << "Unsupported type for FPExt instruction!" << std::endl;
+            srcInst.dump();
+            exit(1);
+          }
+          break;
+        }
+        case llvm::Instruction::FPTrunc: {
+          if (srcInst.getType() == builder.getDoubleTy()) {
+            clonedInst = builder.CreateFPTrunc(VMap[srcInst.getOperand(0)], builder.getFloatTy());
+          } else if (srcInst.getType() == builder.getFloatTy()) {
+            clonedInst = builder.CreateFPTrunc(VMap[srcInst.getOperand(0)], builder.getHalfTy());
+          } else {
+            std::cerr << "Unsupported type for FPTrunc instruction!" << std::endl;
+            srcInst.dump();
+            exit(1);
+          }
+          break;
+        }
+        case llvm::Instruction::Call: {
+          auto CalledFunction = llvm::cast<llvm::CallInst>(&srcInst)->getCalledFunction();
+          std::vector<llvm::Value *> args;
+          for (unsigned i = 0; i < llvm::cast<llvm::CallInst>(&srcInst)->arg_size(); i++) {
+            args.push_back(VMap[llvm::cast<llvm::CallInst>(&srcInst)->getArgOperand(i)]);
+          }
+
+          if (CalledFunction->isIntrinsic()) {
+            if (CalledFunction->getName().contains("f32")) {
+              std::string intrinsicName = CalledFunction->getName().str();
+              intrinsicName.replace(intrinsicName.find("f32"), 3, "f64");
+              llvm::Function *intrinsicFunc = destFunction->getParent()->getFunction(intrinsicName);
+              clonedInst = builder.CreateCall(intrinsicFunc, args);
+            } else {
+              std::cerr << "Unsupported intrinsic function!" << std::endl;
+            }
+          } else {
+            std::cerr << "Unsupported call instruction!" << std::endl;
+            srcInst.dump();
+            exit(1);
+          }
+        }
+        case llvm::Instruction::Ret: {
+          clonedInst = llvm::ReturnInst::Create(context, VMap[llvm::cast<llvm::ReturnInst>(&srcInst)->getReturnValue()], destBlock);
+          break;
+        }
+        default:
+          break;
+      }
+      builder.Insert(clonedInst);
+      VMap[&srcInst] = clonedInst;
+    }
+  }
+
+}
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -52,12 +144,6 @@ int main(int argc, char **argv) {
   auto newModule = std::make_unique<llvm::Module>("main_module", context);
   llvm::IRBuilder<> builder(context);
 
-  // Clone the first function into the new module
-  llvm::ValueToValueMapTy VMap;
-  llvm::Function *clonedFunction = llvm::CloneFunction(firstFunction, VMap);
-  clonedFunction->removeFromParent();
-  newModule->getFunctionList().push_back(clonedFunction);
-
   // Create the main function
   llvm::FunctionType *mainFuncType = llvm::FunctionType::get(builder.getInt32Ty(), false);
   llvm::Function *mainFunc = llvm::Function::Create(
@@ -67,36 +153,159 @@ int main(int argc, char **argv) {
   llvm::BasicBlock *entryBlock = llvm::BasicBlock::Create(context, "entry", mainFunc);
   builder.SetInsertPoint(entryBlock);
 
+
+  // Loop creation
+  llvm::BasicBlock *loopCond = llvm::BasicBlock::Create(context, "loop.cond", mainFunc);
+  llvm::BasicBlock *loopBody = llvm::BasicBlock::Create(context, "loop.body", mainFunc);
+  llvm::BasicBlock *loopEnd = llvm::BasicBlock::Create(context, "loop.end", mainFunc);
+
+  // Define the start, end, and step values
+  llvm::Value *start = llvm::ConstantFP::get(context, llvm::APFloat(1.0f)); // Example: 1.0
+  llvm::Value *end = llvm::ConstantFP::get(context, llvm::APFloat(2.0f));   // Example: 2.0
+
+  // Declare external nextafterf function
+  llvm::FunctionType *nextafterFuncType = llvm::FunctionType::get(builder.getFloatTy(),
+                                                                  {builder.getFloatTy(), builder.getFloatTy()},
+                                                                  false);
+  llvm::Function *nextafterf = llvm::Function::Create(nextafterFuncType,
+                                                      llvm::Function::ExternalLinkage,
+                                                      "nextafterf", newModule.get());
+
+  // Allocate space for the current loop variable
+  llvm::Value *current = builder.CreateAlloca(builder.getFloatTy(), nullptr, "current");
+  builder.CreateStore(start, current);
+  builder.CreateBr(loopCond);
+
+  // Loop condition block
+  builder.SetInsertPoint(loopCond);
+  llvm::Value *currentVal = builder.CreateLoad(builder.getFloatTy(), current, "currentVal");
+  llvm::Value *cond = builder.CreateFCmpOLE(currentVal, end, "cond"); // Compare current <= end
+  builder.CreateCondBr(cond, loopBody, loopEnd);
+
+  // Loop body block
+  builder.SetInsertPoint(loopBody);
+  // Add your loop body operations here
+
+  llvm::Type *retType;
+  std::vector<llvm::Type*> argTypes;
+
+  // Get new return type
+  if(firstFunction->getReturnType()->isHalfTy()) {
+    retType = builder.getFloatTy();
+  } else if(firstFunction->getReturnType()->isFloatTy()) {
+    retType = builder.getDoubleTy();
+  } else {
+    std::cerr << "Unsupported return type ";
+    firstFunction->getReturnType()->print(llvm::errs());
+    std::cerr << " for Validator!" << std::endl;
+    return 1;
+  }
+
+  // Create new argument types
+  for(auto &arg : firstFunction->args()) {
+    if(arg.getType()->isHalfTy()) {
+      argTypes.push_back(builder.getFloatTy());
+    } else if(arg.getType()->isFloatTy()) {
+      argTypes.push_back(builder.getDoubleTy());
+    } else {
+      std::cerr << "Unsupported argument type " << arg.getType() << " for Validator!" << std::endl;
+      return 1;
+    }
+  }
+
+  // Create a type similar to firstFunction but with floating-point types of a higher precision
+  llvm::FunctionType *shadowFunctionType = llvm::FunctionType::get(retType,argTypes,false);
+
+  // Clone the first function into the new module
+  llvm::ValueToValueMapTy VMap;
+  llvm::Function *shadowFunction = llvm::Function::Create(shadowFunctionType,
+                                                          llvm::Function::ExternalLinkage,
+                                                          "shadow_" + firstFunction->getName(), newModule.get());
+
+  createShadowFunction(firstFunction, shadowFunction);
+
+  llvm::Function *clonedFunction = llvm::CloneFunction(firstFunction, VMap);
+  clonedFunction->removeFromParent();
+  newModule->getFunctionList().push_back(clonedFunction);
   // Prepare arguments for the call to the cloned function
   std::vector<llvm::Value*> args;
-  for (auto &arg : clonedFunction->args()) {
+  for (auto &arg : firstFunction->args()) {
     if (arg.getType()->isIntegerTy()) {
       args.push_back(builder.getInt32(0));  // Example: pass 0 for integer arguments
     } else if (arg.getType()->isFloatTy()) {
-      args.push_back(llvm::ConstantFP::get(context, llvm::APFloat(0.0f)));  // Example: pass 0.0 for float
+      args.push_back(llvm::ConstantFP::get(context, llvm::APFloat(1.1f)));  // Example: pass 0.0 for float
     } else if (arg.getType()->isDoubleTy()) {
-      args.push_back(llvm::ConstantFP::get(context, llvm::APFloat(0.0)));  // Example: pass 0.0 for double
+      args.push_back(llvm::ConstantFP::get(context, llvm::APFloat(1.1)));  // Example: pass 0.0 for double
     } else {
       std::cerr << "Unsupported argument type!" << std::endl;
       return 1;
     }
   }
 
-  // Call the cloned function with the prepared arguments
-  builder.CreateCall(clonedFunction, args);
+  std::vector<llvm::Value*> shadowArgs;
+  for (auto &arg : shadowFunction->args()) {
+    if (arg.getType()->isIntegerTy()) {
+      shadowArgs.push_back(builder.getInt32(0));  // Example: pass 0 for integer arguments
+    } else if (arg.getType()->isFloatTy()) {
+      shadowArgs.push_back(llvm::ConstantFP::get(context, llvm::APFloat(1.1f)));  // Example: pass 0.0 for float
+    } else if (arg.getType()->isDoubleTy()) {
+      shadowArgs.push_back(llvm::ConstantFP::get(context, llvm::APFloat(1.1)));  // Example: pass 0.0 for double
+    } else {
+      std::cerr << "Unsupported argument type!" << std::endl;
+      return 1;
+    }
+  }
+
+  // FPExt the return value of the original function to the higher precision
+  llvm::Value *originalFuncCallExt = builder.CreateFPExt(builder.CreateCall(clonedFunction, args), retType);
+  llvm::Value *shadowFuncCall = builder.CreateCall(shadowFunction, shadowArgs);
+
+  // Take the difference of the two function calls
+  llvm::Value *final = builder.CreateFSub(originalFuncCallExt, shadowFuncCall);
+
+  // Declare C standard library printf
+  llvm::FunctionType *printfType = llvm::FunctionType::get(llvm::IntegerType::getInt32Ty(context),
+                                                           llvm::PointerType::getUnqual(llvm::Type::getInt8Ty(context)),
+                                                           true);
+  auto printfFunc = newModule->getOrInsertFunction("printf", printfType);
+
+  auto* formatStr = llvm::ConstantDataArray::getString(context, "%f\n", true);
+  auto* globalVar = new llvm::GlobalVariable(
+          *newModule,
+          formatStr->getType(),
+          true, // Constant
+          llvm::GlobalValue::PrivateLinkage,
+          formatStr,
+          ".str"
+  );
+  llvm::Value* fmtStr = llvm::ConstantExpr::getPointerCast(globalVar,
+                                                           llvm::PointerType::getUnqual(llvm::IntegerType::get(context, 8)));
+
+  // Ensure the doubleValue is properly typed as a variadic argument
+  llvm::Value* doubleAsArg = builder.CreateFPExt(final, llvm::Type::getDoubleTy(context));
+
+  // Call printf
+  builder.CreateCall(printfFunc, {fmtStr, doubleAsArg}, "printfCall");
+
+  // Call nextafterf to calculate the next representable value
+  llvm::Value *nextVal = builder.CreateCall(nextafterf, {currentVal, end}, "nextVal");
+  builder.CreateStore(nextVal, current);
+  builder.CreateBr(loopCond);
 
   // Return 0 in main
+  // Loop end block
+  builder.SetInsertPoint(loopEnd);
   builder.CreateRet(builder.getInt32(0));
 
   // Verify the main function and new module
-//  if (llvm::verifyFunction(*mainFunc, &llvm::errs())) {
-//    std::cerr << "Function verification failed!" << std::endl;
-//    return 1;
-//  }
-//  if (llvm::verifyModule(*newModule, &llvm::errs())) {
-//    std::cerr << "Module verification failed!" << std::endl;
-//    return 1;
-//  }
+  if (llvm::verifyFunction(*mainFunc, &llvm::errs())) {
+    std::cerr << "Function verification failed!" << std::endl;
+    return 1;
+  }
+  if (llvm::verifyModule(*newModule, &llvm::errs())) {
+    std::cerr << "Module verification failed!" << std::endl;
+    return 1;
+  }
 
   // Output the new module to a file
   std::error_code EC;
