68bf8a7a3695ca9d15571219ae42df3e9667d0c8
tanmaytirpankar <tirpankartanmay@gmail.com>
2025-02-11 11:43:36 -0700

    Created top level scripts

--- benchmarks/satire/collect_results.py
+++ benchmarks/satire/collect_results.py
@@ -5,80 +5,110 @@ import glob
 
 fout = open("Results.txt", 'w')
 
-configs = ['noAbs', '10_20', '15_25', '20_40']
+configs = ['noAbs',
+		   # '10_20',
+		   # '15_25',
+		   # '20_40'
+		   ]
 Message = {'noAbs' : 'Without Abstraction ', \
-		   '10_20' : 'Abstraction window (10,20) ',\
-		   '15_25' : 'Abstraction window (15,25) ',\
-		   '20_40' : 'Abstraction window (20,40) ' }
+		   # '10_20' : 'Abstraction window (10,20) ',\
+		   # '15_25' : 'Abstraction window (15,25) ',\
+		   # '20_40' : 'Abstraction window (20,40) '
+		   }
 
-BenchmarkNames = {'poisson2d0' : 'P0', \
+BenchmarkNames = {
+				  'chainSum' : 'Serial Summation', \
+				  'dqmom' : 'Direct Quadrature Method of Moments', \
+				  'jetEngine' : 'Jet Engine', \
+				  'poisson2d0' : 'P0', \
 				  'poisson2d1_t32' : 'P1', \
 				  'poisson2d2_t32' : 'P2', \
 				  'convecdiff2d0' : 'C0', \
 				  'convecdiff2d1_t32' : 'C1', \
 				  'convecdiff2d2_t32' : 'C2', \
+				  'heat1d_t32' : 'H1D', \
 				  'heat2d0_t32' : 'H0', \
 				  'heat2d1_t32' : 'H1', \
 				  'heat2d2_t32' : 'H2', \
 				  'advect3d' : 'Advection3D', \
 				  'fdtd1d_t64' : 'FDTD', \
 				  'lorentz20' : 'lorenz20', \
+				  'lorentz30' : 'lorenz30', \
 				  'lorentz40' : 'lorenz40', \
 				  'lorentz70' : 'lorenz70', \
 				  'matmul64' : 'Matrix Multiplication - 64x64', \
 				  'matmul128' : 'Matrix Multiplication - 128x128', \
+				  'MD' : 'Molecular Dynamics', \
 				  'FFT_1024' : 'FFT_1024', \
 				  'FFT_4096pt' : 'FFT_4096', \
 				  'Scan_1024' : 'Scan_1024(Prefix sum)', \
 				  'Scan_4096' : 'Scan_4096(Prefix Sum)', \
 				  'CG_Arc' : 'Conjugate gradient (ARC130)', \
+				  'CG_EX5' : 'Conjugate gradient (EX5)', \
 				  'CG_Pores' : 'COnjugate gradient (Pores)', \
-				  'var_ccsd_type2_0' : 'Tensor Contraction-type0', \
-				  'var_ccsd_type2_1' : 'Tensor Contraction-type1', \
-				  'ccsd_type2_0' : 'Tensor Contraction-type0 (Degenerate Intervals)', \
-				  'ccsd_type2_1' : 'Tensor Contraction-type1 (Degenerate Intervals)' }
+				  'CG_Tref' : 'COnjugate gradient (Tref)', \
+				  'varccsd_type0_0' : 'Tensor Contraction-type01', \
+				  'varccsd_type0_1' : 'Tensor Contraction-type01', \
+				  'varccsd_type1_0' : 'Tensor Contraction-type10', \
+				  'varccsd_type1_1' : 'Tensor Contraction-type11', \
+				  'var_ccsd_type2_0' : 'Tensor Contraction-type20', \
+				  'var_ccsd_type2_1' : 'Tensor Contraction-type21', \
+				  'ccsd_type0_1' : 'Tensor Contraction-type01 (Degenerate Intervals)', \
+				  'ccsd_type1_0' : 'Tensor Contraction-type10 (Degenerate Intervals)', \
+				  'ccsd_type1_1' : 'Tensor Contraction-type11 (Degenerate Intervals)', \
+				  'ccsd_type2_0' : 'Tensor Contraction-type20 (Degenerate Intervals)', \
+				  'ccsd_type2_1' : 'Tensor Contraction-type21 (Degenerate Intervals)', \
+				  'poly-eval' : 'Polynomial Evaluation', \
+				  'reduction' : 'Reduction', \
+				  'Scan_1024' : 'Scan Sum 1024 points', \
+				  'Scan_4096' : 'Scan Sum 4096 points', \
+				  'wave2d1' : 'Wave sim',
+	}
 
 test_name = sys.argv[1]
 print(test_name)
 fout.write(test_name+"\n")
 file_list = list(glob.iglob('*'))
 
-pylog_dict = dict()
-outlog_dict = dict()
-#print(file_list)
-pylog_dict['10_20'] = list(filter( lambda x: 'pylog' in x and '10_20' in x, file_list))
-pylog_dict['15_25'] = list(filter( lambda x: 'pylog' in x and '15_25' in x, file_list))
-pylog_dict['20_40'] = list(filter( lambda x: 'pylog' in x and '20_40' in x, file_list))
-pylog_dict['noAbs'] = list(filter( lambda x: 'pylog' in x and 'noAbs' in x, file_list))
+clog_dict = dict()
+csv_dict = dict()
+print(file_list)
+# clog_dict['10_20'] = list(filter( lambda x: 'clog' in x and '10_20' in x, file_list))
+# clog_dict['15_25'] = list(filter( lambda x: 'clog' in x and '15_25' in x, file_list))
+# clog_dict['20_40'] = list(filter( lambda x: 'clog' in x and '20_40' in x, file_list))
+clog_dict['noAbs'] = list(filter( lambda x: 'clog' in x and 'noAbs' in x, file_list))
+print(clog_dict)
 
-outlog_dict['10_20'] = list(filter(lambda x: 'out' in x and '10_20' in x , file_list))
-outlog_dict['15_25'] = list(filter(lambda x: 'out' in x and '15_25' in x , file_list))
-outlog_dict['20_40'] = list(filter(lambda x: 'out' in x and '20_40' in x , file_list))
-outlog_dict['noAbs'] = list(filter(lambda x: 'out' in x and 'noAbs' in x , file_list))
-#print(pylog_dict)
+# csv_dict['10_20'] = list(filter(lambda x: 'csv' in x and '10_20' in x , file_list))
+# csv_dict['15_25'] = list(filter(lambda x: 'csv' in x and '15_25' in x , file_list))
+# csv_dict['20_40'] = list(filter(lambda x: 'csv' in x and '20_40' in x , file_list))
+csv_dict['noAbs'] = list(filter(lambda x: 'csv' in x and 'noAbs' in x , file_list))
+#print(clog_dict)
 BenchName = BenchmarkNames.get(test_name, test_name)
 print("****** Benchmark :", BenchName, "**************")
 fout.write("****** Benchmark : {bench} **************\n".format(bench=BenchName))
 for conf in configs:
-	if (len(pylog_dict[conf]) == 0):
+	if (len(clog_dict[conf]) == 0):
 		print(Message[conf], "did not execute \n")
-		fout.write("{message} -- did not execute \n".format(message=Message[conf]))
+		fout.write("{message} -- FAILED to execute \n".format(message=Message[conf]))
 	else:
-		pylogname = pylog_dict[conf][0]
-		outlogname = outlog_dict[conf][0]
-		#print(pylogname, outlogname)
-		logfile = open(pylogname, 'r').read().splitlines()
-		outfile = open(outlogname, 'r').read().splitlines()
-		AST_DEPTH = list(filter(lambda x: "AST_DEPTH" in x, logfile))
-		ABSOLUTE_ERROR = list(filter(lambda x: "ABSOLUTE_ERROR" in x, outfile))
-		EXECUTION_TIME = list(filter(lambda x: "Full time" in x, outfile))
-
-		error = max(list(map(lambda x : float(x.split(':')[1]), ABSOLUTE_ERROR)))
-
-		print("\t",Message[conf], "->", "execution time :", EXECUTION_TIME[0])
-		print("\t",Message[conf], "->", "absolute error :", error)
-		fout.write("\t {message} -->  execution time = {exec_time}\n".format(message=Message[conf], exec_time=EXECUTION_TIME[0]))
-		fout.write("\t {message} -->  absolute error = {abs_err}\n\n\n".format(message=Message[conf], abs_err=error))
+		clogname = clog_dict[conf][0]
+		csvname = csv_dict[conf][0]
+		#print(clogname, csvname)
+		logfile = open(clogname, 'r').read().splitlines()
+		csvfile = open(csvname, 'r').read().splitlines()
+		fout.write("{message} -- SUCCESSFULLY executed \n".format(message=Message[conf]))
+		# AST_DEPTH = list(filter(lambda x: "AST_DEPTH" in x, logfile))
+		# ABSOLUTE_ERROR = list(filter(lambda x: "ABSOLUTE_ERROR" in x, csvfile))
+		# EXECUTION_TIME = list(filter(lambda x: "Total Time" in x, csvfile))
+		#
+		# error = max(list(map(lambda x : float(x.split(':')[1]), ABSOLUTE_ERROR)))
+		#
+		# print("\t",Message[conf], "->", "execution time :", EXECUTION_TIME[0])
+		# print("\t",Message[conf], "->", "absolute error :", error)
+		# fout.write("\t {message} -->  execution time = {exec_time}\n".format(message=Message[conf], exec_time=EXECUTION_TIME[0]))
+		# fout.write("\t {message} -->  absolute error = {abs_err}\n\n\n".format(message=Message[conf], abs_err=error))
+		pass
 
 
 fout.close()
--- benchmarks/satire/runScript.sh
+++ benchmarks/satire/runScript.sh
@@ -1,117 +1,88 @@
 #!/bin/bash
-#SBATCH --time=72:00:00
-#SBATCH --partition=soc-kp
 #SBATCH --account=soc-kp
-#SBATCH -N 1 
-#SBATCH -J benchmarks_abs-serial-sim
-#SBATCH -o benchmarks-abs-serial-sim.log
-#SBATCH -e benchmarks-abs-serial-sim.log
-#set CC=/uufs/chpc.utah.edu/sys/installdir/gcc/5.4.0/bin/gcc
-#set CXX=/uufs/chpc.utah.edu/sys/installdir/dcc/5.4.0/bin/g++
-source /uufs/chpc.utah.edu/common/home/u1014840/local_python3.8
+#SBATCH --partition=soc-kp
+#SBATCH --job-name=benchmarks_abs
+#SBATCH -o benchmarks-abs.log
+#SBATCH -e benchmarks-abs.log
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=72:00:00
+#SBATCH --export=ALL
+#SBATCH --qos=soc-kp
 
 
-DIRS="FFT_1024\
-	lorentz20\
+DIRS="
 	poisson2d0\
 	poisson2d1_t32\
 	poisson2d2_t32\
 	convecdiff2d0\
 	convecdiff2d1_t32\
 	convecdiff2d2_t32\
+	heat1d_t32\
 	heat2d0_t32\
 	heat2d1_t32\
 	heat2d2_t32\
 	advect3d\
 	fdtd1d_t64\
 	lorentz20\
+	lorentz30\
 	lorentz40\
 	lorentz70\
 	matmul64\
 	matmul128\
 	FFT_1024\
 	FFT_4096pt\
+	dqmom\
 	MD\
 	chainSum\
 	poly-eval\
 	horner\
+  jetEngine\
 	reduction\
 	Scan_1024\
 	Scan_4096\
 	CG_Arc\
+	CG_EX5\
 	CG_Pores\
+	CG_Tref\
+	var_ccsd_type0_0\
+  var_ccsd_type0_1\
+  var_ccsd_type1_0\
+  var_ccsd_type1_1\
 	var_ccsd_type2_0\
 	var_ccsd_type2_1\
+  ccsd_type0_1\
+  ccsd_type1_0\
+  ccsd_type1_1\
 	ccsd_type2_0\
-	ccsd_type2_1"
-
-#DIRS="horner\
-#	 reduction\
-#	 poly-eval"
+	ccsd_type2_1\
+	wave2d1
+	"
 
 set -x
 
-echo "Here: $GPHOME"
-#GPHOME="/scratch/general/lustre/u1014840/gelpia/"
-GPHOME1="/scratch/general/lustre/u1014840/gelpia/"
-
-# use only if $GPHOME is not defined globally else use the global env value
-if [[ -z "${GPHOME}$" ]]; then
-
-	if [[ -d $GPHOME1 ]]; then
-		GPHOME=$GPHOME1
-	else
-		echo "GPHOME1 is set to $GPHOME1 : no such directory --  Fix GPHOME path to gelpia home directory"
-	fi
-else if [[ -d "${GPHOME}$" ]]; then
-		echo "$GPHOME is a valid directory"
-	 else
-		if [[ -d $GPHOME1 ]]; then
-			GPHOME=$GPHOME1
-		else
-			echo "GPHOME1 is set to $GPHOME1 : no such directory --  Fix GPHOME path to gelpia home directory"
-		fi
-	 	
-	 fi
-fi
-
-GPEXE1=$GPHOME/bin/gelpia
-
-GPEXE2=$(eval "which gelpia")
-
-#EXE1 = $(eval "sed -i s/\/\//\//g $GPEXE1")
-#EXE2 = $(eval "sed -i s/\/\//\//g $GPEXE2")
-
-EXE1=$(eval "echo ${GPEXE1//\/\//\/}")
-EXE2=$(eval "echo ${GPEXE2//\/\//\/}")
-
-if [[ $EXE1 == $EXE2 ]];
-then
 
 #find $GPHOME -name "*generated*"
 
-	if [[ -f Results.txt ]]
-	then
-		echo "Removing old results file"
-		rm -rf Results.txt
-	fi
-	
-	for d in $DIRS
-	do
-		echo $d
-		cd $d
-		echo "executing batch scipt for " $d
-		bash batch_abs.slurm
-		python3 ../collect_results.py $d
-		echo $PWD
-		cat Results.txt >> ../Results.txt
-		echo "Cleaning up gelpia generated files"
-		find $GPHOME -name "*generated*" | xargs rm -rf
-		cd ..
-	done
-	
-	echo "Open Results.txt for summary of the evaluation"
-else
-	echo "COrrectly set 'GPHOME' to gelpia home diretcory"
+if [[ -f Results.txt ]]
+then
+  echo "Removing old results file"
+  rm -rf Results.txt
 fi
+
+for d in $DIRS
+do
+  echo $d
+  cd $d
+  echo "executing batch scipt for " $d
+  bash batch_abs.slurm
+  python3 ../collect_results.py $d
+  echo $PWD
+  cat Results.txt >> ../Results.txt
+  cd ..
+done
+
+echo "Open Results.txt for summary of the evaluation"
+
 exit
